2e016f5d92d607a9d75d3d2c418cd37e7ae92f6b
Vladii <vladii@Vladiis-MacBook-Air.local>
2018-10-09 21:25:36 +0300

    made a random walk with constructors

--- Random_Walk/main.js
+++ Random_Walk/main.js
@@ -4,16 +4,30 @@ var y = 600 / 2;
 
 function setup() {
   createCanvas(900, 600);
+  w = new Walker();
   background(153,111,214);
 }
 
 function draw() {
-  fill(0);
-  rect(x, y, 2, 2);
-  x += random(-1, 1);
-  y += random(-1, 1);
+  w.display();
+  w.walk();
 }
 
 function mousePressed(){
   background(153,111,214);
+}
+
+function Walker(){
+  this.x = width/2;
+  this.y = height/2;
+
+  this.walk = function(){
+    this.x += random(-1,1);
+    this.y += random(-1,1);
+  }
+
+  this.display = function(){
+    fill(0);
+    rect(this.x, this.y, 2, 2);
+  }
 }
